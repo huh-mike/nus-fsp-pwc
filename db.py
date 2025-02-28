63c05682306f3ebee602ba9a13b89ac456dfee32
31245191 <nehabhakat19@gmail.com>
2025-02-28 11:30:13 +0800

    updated - neha

--- db.py
+++ db.py
@@ -5,31 +5,18 @@ import logging
 import json
 import ssl
 
+def upload_to_mongo(data, db_name, collection_name):
+    MONGO_URI = "mongodb://nehabhakat:kMxFQYPZ5Z5rqHOEVJx2BPToWVyHpmmrpnl9rniu6AokTNMO3TsAhHXeFvQaEv3lNqeTNXctc2EQACDbthoOWw==@nehabhakat.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nehabhakat@"
+    client = MongoClient(MONGO_URI, tls=True, tlsAllowInvalidCertificates=True)
+    db = client[db_name]
+    collection = db[collection_name]
 
-def upload_to_mongo(data, db_name, collection_name,
-                    mongo_uri="mongodb://nehabhakat:kMxFQYPZ5Z5rqHOEVJx2BPToWVyHpmmrpnl9rniu6AokTNMO3TsAhHXeFvQaEv3lNqeTNXctc2EQACDbthoOWw==@nehabhakat.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nehabhakat@"):
-    """
-    Uploads tagged data to MongoDB on Azure.
-    :param data: List of dictionaries containing tagged data.
-    :param db_name: Name of the database.
-    :param collection_name: Name of the collection.
-    :param mongo_uri: MongoDB connection URI.
-    """
     try:
-        client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
-        db = client[db_name]
-        collection = db[collection_name]
-
-        if isinstance(data, list):
-            collection.insert_many(data)
-        else:
-            collection.insert_one(data)
-
-        print("Data successfully uploaded to MongoDB on Azure.")
+        result = collection.insert_many(data)
+        print(f"Uploaded {len(result.inserted_ids)} documents to {collection_name}")
     except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        client.close()
+        print(f"An error occurred while uploading to MongoDB: {e}")
+
 
 
 if __name__ == "__main__":
@@ -39,18 +26,10 @@ if __name__ == "__main__":
         {"id": 2, "text": "Another example with tags.", "tags": ["example", "tags"]}
     ]
 
-    # Upload to MongoDB on Azure
     upload_to_mongo(tagged_data, "TaggedDatabase", "TaggedCollection")
 
 def fetch_from_mongo(db_name, collection_name, query={}, mongo_uri="mongodb://nehabhakat:kMxFQYPZ5Z5rqHOEVJx2BPToWVyHpmmrpnl9rniu6AokTNMO3TsAhHXeFvQaEv3lNqeTNXctc2EQACDbthoOWw==@nehabhakat.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@nehabhakat@"):
-    """
-    Fetches data from MongoDB based on a query.
 
-    :param db_name: Name of the database.
-    :param collection_name: Name of the collection.
-    :param query: Dictionary query for filtering results.
-    :param mongo_uri: MongoDB connection URI.
-    """
     try:
         client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
         db = client[db_name]
